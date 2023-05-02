a7cdaaee95d204ad4abfec49b538c493d5e0a066
Amankulova-Aiperi <aiperi.amankulovaa@gmail>
2023-05-02 10:26:51 +0600

    add js file

---
+++ add.js
@@ -0,0 +1,6 @@
+document.querySelector("#open_sidebar").addEventListener("click", function() {
+    document.querySelector("#sidebar").classList.add("show");
+  });
+document.querySelector("#close_sidebar").addEventListener("click", function() {
+    document.querySelector("#sidebar").classList.remove("show");
+  });
